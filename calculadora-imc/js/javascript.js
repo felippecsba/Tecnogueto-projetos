62aee9f265fe04e2be7a3a717be2c3d86ed2a1a8
Felippe Araújo <felippecsbaraujo@gmail.com>
2021-06-05 00:36:15 -0300

    ajustando as funções da calculadora

--- calculadora-imc/js/javascript.js
+++ calculadora-imc/js/javascript.js
@@ -1,19 +1,62 @@
 const botaoCalcular = document.getElementById("botaoCalcular");
-
 botaoCalcular.addEventListener("click", function() {
+    validarForms();
+});
+
+function msgResultadoCalcIMC(nome, peso, altura) {
+    const resultadoDoCalculoIMC = (peso / (altura * altura)).toFixed(1);
+    let classificaco = '';
+
+    if (resultadoDoCalculoIMC < 18.5 ) {
+        classificaco = "Abaixo do peso"
+    } else if (resultadoDoCalculoIMC < 25 ) {
+        classificaco = "Peso ideal"
+    } else if (resultadoDoCalculoIMC < 30 ) {
+        classificaco = "Acima de peso"
+    } else if (resultadoDoCalculoIMC < 35 ) {
+        classificaco = "Com obseidade grau I"
+    } else if (resultadoDoCalculoIMC >= 35 ) {
+        classificaco = "Com obesidade alta "
+    }
+
+    showMensagemSucesso(nome, resultadoDoCalculoIMC, classificaco);
+
+}
+
+function validarForms() {
     const nome = document.getElementById("nome").value;
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
 
+    const convertPesoNum = parseInt(peso);
+    const convertAlturaNum =  parseInt(altura);
+
+    if (nome != '' && altura != '' && peso != '') {
+        if (Number.isInteger(convertAlturaNum) && Number.isInteger(convertPesoNum) ) {
+
+            msgResultadoCalcIMC(nome, peso, altura);
+
+        } else {
+            showMesangemErro (`Os campos Altura e Peso  precisam ser número`); 
+        }
+    } else {   
+        showMesangemErro (`Preencha todos os campos`);
+    }
+
+}
+
+function showMensagemSucesso(nome, resultadoCalculo, classificaco) {
     const mensagemSucesso = document.getElementById("mensagemSucesso");
-
-    const resultadoDoCalculoIMC = (peso / (altura * altura)).toFixed(1);
-
-    
-    mensagemSucesso.textContent = `${nome} seu IMC é: ${resultadoDoCalculoIMC}`;
+    mensagemSucesso.textContent = `${nome} seu IMC é: ${resultadoCalculo}, e você está ${classificaco}`;
     mensagemSucesso.setAttribute("class", "alert alert-primary mt-5 mb-5 mostrar");
+    mensagemErro.setAttribute("class", "esconder");
+}
 
+function showMesangemErro(msgErro) {   
+    const mensagemErro = document.getElementById("mensagemErro"); 
+    mensagemErro.textContent = msgErro;
+    mensagemErro.setAttribute("class", "alert alert-danger mt-5 mb-5 mostrar");
+    mensagemSucesso.setAttribute("class", "esconder");
 
-});
+}
 
-// peso / (altura * altura)
